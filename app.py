1825742914f6caea9d129f0f498b5c60e8593ad8
Matt Grogan <johnmattgrogan@gmail.com>
2015-06-15 11:02:58 -0500

    added comments

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
+# ----Flask Hello World---
+
+# import the Flask class from the flask
 from flask import Flask
 
+
+# create the application object
 app = Flask(__name__)
 
+# use decorators to link the function to a url
 @app.route("/")
 @app.route("/home")
 def hello_world():
 	return "Hello, World!"
 
-
+# start the development server using run() method
 if __name__ == "__main__":
 	app.run()
 	
